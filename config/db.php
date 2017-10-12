bd1c655d05569f524156d126c27a6aa8c6d958bd
Dwi Nurhadi Afriyanto <dwi.nurhadi17@student.uns.ac.id>
2017-10-13 05:45:40 +0700

    query select yii2

--- config/db.php
+++ config/db.php
@@ -1,9 +1,10 @@
 <?php
 
+//CREATE USER 'monty'@'10.70.%.%' IDENTIFIED BY 'some_pass';
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=10.13.245.106;dbname=soccerdb',
+    'username' => 'monty',
+    'password' => 'some_pass',
     'charset' => 'utf8',
 ];
---
+++ views/hello-database/query-builder-index.php
@@ -0,0 +1,151 @@
+<?php
+$this->title = 'Demo Databases Query';
+?>
+
+<h1>Daftar tim bal balan</h1>
+<p>data berikut diambil dari database soccerdb</p>
+
+<pre>
+$query = (new \yii\db\Query())
+        ->select('*')
+        ->from('teams')
+        ->all();
+</pre>
+<p>Hasil <?= count($query1) ?> rows</p>
+
+<table class="table table-bordered table-striped"
+    <thead>
+        <tr>
+            <td>ID</td>
+            <td>Name</td>
+            <td>Country</td>
+            <td>Descriptions</td>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($query1 as $team): ?>
+        <tr>
+            <td><?= $team['id'] ?></td>
+            <td><?= $team['name'] ?></td>
+            <td><?= $team['country'] ?></td>
+            <td><?= $team['description'] ?></td>
+        </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+<hr>
+<pre>
+$query2 = (new \yii\db\Query())
+                    ->select(['id', 'name', 'country'])
+                    ->from('teams')
+                    ->limit(5)
+                    ->all();
+</pre>
+<p>==> return <?= count($query2) ?> rows</p>
+<table class="table table-bordered table-striped">
+    <thead>
+        <tr>
+            <td>ID</td>
+            <td>Name</td>
+            <td>Country</td>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($query2 as $team): ?>
+            <tr>
+                <td><?= $team['id'] ?></td>
+                <td><?= $team['name'] ?></td>
+                <td><?= $team['country'] ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+
+<hr />
+<pre>
+$query3 = (new \yii\db\Query())
+                    ->select(['*'])
+                    ->from('teams')
+                    ->where(['country' => 'England'])
+                    ->orderBy('name')
+                    ->all();
+</pre>
+<p>==> return <?= count($query3) ?> rows</p>
+<table class="table table-bordered table-striped">
+    <thead>
+        <tr>
+            <td>ID</td>
+            <td>Name</td>
+            <td>Country</td>
+            <td>Description</td>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($query3 as $team): ?>
+            <tr>
+                <td><?= $team['id'] ?></td>
+                <td><?= $team['name'] ?></td>
+                <td><?= $team['country'] ?></td>
+                <td><?= $team['description'] ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+
+<hr />
+<pre>
+$query4 = (new \yii\db\Query())
+                    ->select(['*'])
+                    ->from('teams')
+                    ->where(['description' => '%sebut%'])
+                    ->orderBy('name')
+                    ->all();
+</pre>
+<p>==> return <?= count($query4) ?> rows</p>
+<table class="table table-bordered table-striped">
+    <thead>
+        <tr>
+            <td>ID</td>
+            <td>Name</td>
+            <td>Country</td>
+            <td>Description</td>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($query4 as $team): ?>
+            <tr>
+                <td><?= $team['id'] ?></td>
+                <td><?= $team['name'] ?></td>
+                <td><?= $team['country'] ?></td>
+                <td><?= $team['description'] ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+
+<hr />
+<pre>
+$query5 = (new \yii\db\Query())
+                    ->select(['country'])
+                    ->from('teams')
+                    ->groupBy('country')
+                    ->orderBy('name')
+                    ->all();
+</pre>
+<p>==> return <?= count($query5) ?> rows</p>
+<table class="table table-bordered table-striped">
+    <thead>
+        <tr>
+            <td>Country</td>
+            <td>Total</td>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($query5 as $team): ?>
+            <tr>
+                <td><?= $team['country'] ?></td>
+                <td><?= $team['total'] ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
