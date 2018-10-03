5e8652ea7b9815de63499a43670ec29f14363360
Baskoro Sinelir <baskorosinelir2@gmail.com>
2018-10-03 17:03:34 +0700

    edit greeting

--- views/hello-view/greeting.php
+++ views/hello-view/greeting.php
@@ -3,6 +3,6 @@ $this->title = "Hello View-View ini ada dalam folder greeting";
 
 ?>
 
-<h1>Welcome</h1>
+<h1>Welcome...</h1>
 
 <p>Halloo <?= $salutation ?>, dengan <?= $name ?>, dari <?= $address ?></p>
